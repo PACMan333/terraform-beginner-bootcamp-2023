5bd9634d2133e0a4de7a05b6b9ae83916c301ca7
PACMan333 <p_cozza@yahoo.com>
2023-09-30 18:00:06 +0000

    ip

--- modules/terrahouse_aws/variables.tf
+++ modules/terrahouse_aws/variables.tf
@@ -28,7 +28,7 @@ variable "index_html_filepath" {
     error_message = "The provided path for index.html does not exist."
   }
 }
-
+/*
 variable "error_html_filepath" {
   description = "The file path for error.html"
   type        = string
@@ -38,4 +38,4 @@ variable "error_html_filepath" {
     error_message = "The provided path for error.html does not exist."
   }
 }
-
+*/
