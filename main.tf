6b62622d591dbf1d257a103887666aefa0f2f0f6
PACMan333 <37124293+PACMan333@users.noreply.github.com>
2023-10-04 19:08:56 -0400

    fix issues with provider, setup two different towns (#25)

--- main.tf
+++ main.tf
@@ -5,13 +5,13 @@ terraform {
      version = "1.0.0"
    }
   } 
-  #cloud {
-  #  organization = "PACSoft"
-  # 
-  #  workspaces {
-  #    name = "terra-house-1"
-  #  }
-  #}
+  cloud {
+    organization = "PACSoft"
+   
+    workspaces {
+      name = "terra-house-1"
+    }
+  }
 
 }
 
@@ -26,13 +26,11 @@ provider "terratowns" {
 }
 
 
-module "terrahouse_aws" {
+module "home_arcanum_hosting" {
   source = "./modules/terrahouse_aws"
   user_uuid = var.teacherseat_user_uuid
-  index_html_filepath = var.index_html_filepath
-  error_html_filepath = var.error_html_filepath
-  content_version = var.content_version
-  assets_path = var.assets_path
+  public_path = var.arcanum.public_path
+  content_version = var.arcanum.content_version
 }
 
 
@@ -45,9 +43,28 @@ to play (despite that old look graphics). This is my guide that will
 show you how to play arcanum without spoiling the plot.
 DESCRIPTION
   #domain_name = module.home_arcanum_hosting.domain_name
-  domain_name = module.terrahouse_aws.cloudfront_url
+  domain_name = module.home_arcanum_hosting.domain_name
   #content_version = var.arcanum.content_version
   #domain_name = "3fdq33gz.cloudfront.net"
   town = "missingo"
-  content_version = 1
+  content_version = var.arcanum.content_version
+}
+
+
+module "home_payday_hosting" {
+  source = "./modules/terrahouse_aws"
+  user_uuid = var.teacherseat_user_uuid
+  public_path = var.payday.public_path
+  content_version = var.payday.content_version
+}
+
+resource "terratowns_home" "home_payday" {
+  name = "Making your own Payday Bars"
+  description = <<DESCRIPTION
+Since Payday candy bars are really good, I decided I would see how I could make my own Payday bars,
+and if they taste good.
+DESCRIPTION
+  domain_name = module.home_payday_hosting.domain_name
+  town = "missingo"
+  content_version = var.payday.content_version
 }
